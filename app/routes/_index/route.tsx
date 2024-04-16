4c86004ae4a1e291c45d98840700591ff4cd8afb
Josh Thiele <j.thiele@duttongroup.com.au>
2024-04-16 14:19:44 +1000

    added header

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -4,7 +4,7 @@ import classes from './_index.module.css'
 import background from '~/assets/images/background.jpg'
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
+  return [{ title: `Josh & Nathan's Wedding` }, { name: 'description', content: 'Our High Fantasy Wedding' }]
 }
 
 export default function Index() {
@@ -14,14 +14,13 @@ export default function Index() {
         <img className={classes.background} src={background} alt='background' />
         <div className={classes.overlay}>
           <h2>Welcome to Josh and Nathan&apos;s</h2>
-          <h1>High- Fantasy Wedding</h1>
-          <Link to='/rsvp'>RSVP Here</Link>
-          <Link to='/admin'>Admin Click Here</Link>
+          <h1>High -Fantasy Wedding</h1>
+          <Link to='/rsvp'>Want to RSVP? Click Here</Link>
         </div>
       </div>
-
       <div className={classes.content}>
         <h1>Buttons</h1>
+        <Link to='/admin'>Admin Click Here</Link>
       </div>
     </div>
   )
